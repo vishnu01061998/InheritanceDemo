1a1b72b3a46791c42ea351e54217680667cd7c77
HP <HP@DESKTOP-4Q8V1JF>
2023-04-05 01:32:11 +0530

    Add project files.

---
+++ Demo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    public class Teacher
+    {
+        public void GetData()
+        {
+        }
+        public void PutData()
+        {
+
+        }
+    }
+
+
+    public class ParamanatTeacher : Teacher
+    {
+        void function1()
+        {
+
+        }
+    }
+
+
+    internal class parent
+    {
+
+        public parent()  // constructor
+        {
+            this.Id = 10;
+        }
+        // Data member
+        public int Id { get; set; }
+
+        // member function 
+        public void function1()
+        {
+
+            Console.WriteLine("parent class function 1");
+        }
+        public void function2()
+        {
+            Console.WriteLine("parent class function 2");
+        }
+    }
+    internal class Child : parent
+    {
+
+        // constructor
+        public Child()
+        {
+              this.Name  = "Vaish";
+        }
+
+         // date members
+         public string Name { get; set; }
+
+
+        // member function 
+        public void function3()
+        {
+            Console.WriteLine("Child class function 3");
+
+            Console.WriteLine("Name=" + Name);
+            Console.WriteLine("Id=" + Id);
+
+            
+
+        }
+        public void function4(int num1, int num2, string operation)  //Formal paramters
+        {
+
+            if (operation == "Add")
+            {
+                int result = num1 + num2;
+            }
+
+
+            if (operation == "Sub")
+            {
+                int result = num1 - num2;
+            }
+
+
+            Console.WriteLine("Child class function 4 ");
+        }
+
+        
+        
+           
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Child C = new Child();
+            parent p = new parent();
+
+
+            Child c1;
+            parent p1;
+
+            // c1= p; // invalid
+            p1 = C;  // Valid
+
+            // p1 is the variable of parent class but its the refernce of child class
+
+            // p1.function3();
+
+
+
+
+            //Child C;   //This is Variable
+
+            //C = new Child();//Instance of the class
+
+
+            //Child C2 = new Child();  //instace of the class
+
+            //Child C3;   //variable of the class
+            //C3 = C2;   //Reference of the class
+            
+        }
+    }
+}
